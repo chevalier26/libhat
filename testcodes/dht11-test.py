acf5621eff530ff44ba8a398d0aa0de7142df84e
archivistnathan <nathan@slis.upd.ph>
2019-11-08 09:33:55 +0000

    Test scripts for MCP3008 from Adafruit

--- testcodes/dht11-test.py
+++ testcodes/dht11-test.py
@@ -1,15 +1,21 @@
+import RPi.GPIO
 import Adafruit_DHT
- 
+print('Library import success') 
+
 # Set sensor type : Options are DHT11,DHT22 or AM2302
 sensor=Adafruit_DHT.DHT11
+print('Sensor assigned')
  
 # Set GPIO sensor is connected to
 gpio=17
+print('GPIO selected')
  
 # Use read_retry method. This will retry up to 15 times to
 # get a sensor reading (waiting 2 seconds between each retry).
+print('Attempting Query')
 humidity, temperature = Adafruit_DHT.read_retry(sensor, gpio)
- 
+print('Attempt Complete')
+
 # Reading the DHT11 is very sensitive to timings and occasionally
 # the Pi might fail to get a valid reading. So check if readings are valid.
 if humidity is not None and temperature is not None:

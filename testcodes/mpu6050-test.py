303d968015d1b3502d1be38cb9fe6c41638d41fa
Nathan <56992436+archivistnathan@users.noreply.github.com>
2019-11-15 09:06:43 +0800

    Update mpu6050-test.py

--- testcodes/mpu6050-test.py
+++ testcodes/mpu6050-test.py
@@ -78,5 +78,5 @@ while True:
 	Gz = gyro_z/131.0
 	
 
-	print ("Gx=%.2f" %Gx, + "deg/s", "Gy=%.2f" %Gy, + "deg/s", "Gz=%.2f" %Gz, + "deg/s", "Ax=%.2f g" %Ax, "Ay=%.2f g" %Ay, "Az=%.2f g" %Az) 	
+	print ("Gx=%.2f" %Gx, + "deg/s , Gy=%.2f" %Gy, + "deg/s , Gz=%.2f" %Gz, + "deg/s , Ax=%.2f g" %Ax, + "Ay=%.2f g" %Ay, + "Az=%.2f g" %Az) 	
 	sleep(1)

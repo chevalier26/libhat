2cc6451ea3a0db11d68e764fc417f9857d923e77
arvinjason <arvinjasonaquino@gmail.com>
2019-12-10 15:25:30 +0800

    Updated

--- testcodes/userCount.py
+++ testcodes/userCount.py
@@ -2,22 +2,26 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(12, GPIO.IN)         #Read output from PIR motion sensor
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.IN)         #Read output from PIR motion sensor
 
 
 global user_counter
 user_counter = 0
 
+print "Start sequence"
+print user_counter
+
+
 while True:
-	i=GPIO.input(12)
+	i=GPIO.input(18)
 if i==0:                 #When output from motion sensor is LOW
 	print "No intruders",i
-
-	time.sleep(0.1)
+	print user_counter
+	time.sleep(0.5)
 
 elif i==1:               #When output from motion sensor is HIGH
 	print "Intruder detected",i
-
 	user_counter+=1
-	time.sleep(0.1)
+	print user_counter
+	time.sleep(0.5)

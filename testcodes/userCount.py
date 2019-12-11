1fd93813ed16efb866a2b117106014a2b6a01275
aldrinken <38404169+aldrinken@users.noreply.github.com>
2019-12-11 12:09:24 +0800

    Update userCount.py

--- testcodes/userCount.py
+++ testcodes/userCount.py
@@ -17,15 +17,15 @@ print user_counter
 
 while True:
 	i=GPIO.input(18)
-if i==0:                 #When output from motion sensor is LOW
-	print "No intruders, (",i, ")"
-	print user_counter
-	time.sleep(1)
-	delayChecker = 0
+	if i==0:                 #When output from motion sensor is LOW
+		print "No intruders, (",i, ")"
+		print user_counter
+		time.sleep(1)
+		delayChecker = 0
 
-else if i==1:               #When output from motion sensor is HIGH
-	print "Intruder detected, (", i, ")"
-	user_counter+=1
-	print "People: ", user_counter#, "\n"
-	print "[Delay: ", delayChecker, "]", "\n"
-	time.sleep(1)
+	else if i==1:               #When output from motion sensor is HIGH
+		print "Intruder detected, (", i, ")"
+		user_counter+=1
+		print "People: ", user_counter#, "\n"
+		print "[Delay: ", delayChecker, "]", "\n"
+		time.sleep(1)

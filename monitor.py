a1c4fb54e0117293bcb1fdff25b182f8b6658800
chevalier26 <57433629+chevalier26@users.noreply.github.com>
2019-12-11 14:44:01 +0800

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,7 +1,9 @@
 from temphummod import readtemphumid
 from shock import seismometer
 from userCount import userCount
+from SoundSensor import read_sound
 
 readtemphumid()
 seismometer()
 userCount()
+read_sound()

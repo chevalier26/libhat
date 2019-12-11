ca8f1257242134d7b9d13865bc41608552d69f1a
imaerafukien <57431975+imaerafukien@users.noreply.github.com>
2019-12-11 15:21:14 +0800

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -2,14 +2,14 @@ from temphummod import readtemphumid
 from shock import seismometer
 from userCount import userCount
 from SoundSensor import read_sound
-#from lightSensor import read_light
-#import threading
-#from threading import Thread
+from lightSensor import read_light
+import threading
+from threading import Thread
 
 
 readtemphumid()
 seismometer()
 userCount()
 read_sound()
-#light = threading.Thread(target=read_light, args=())
-#light.start()
+light = threading.Thread(target=read_light, args=())
+light.start()

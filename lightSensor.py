9ca982c4117fc8708fa2eb1178427b5cb9091e72
imaerafukien <57431975+imaerafukien@users.noreply.github.com>
2019-12-11 13:43:50 +0800

    Update lightSensor.py
    changed LED blink interval from 0.5 to 2 seconds.

--- lightSensor.py
+++ lightSensor.py
@@ -59,7 +59,7 @@ def read_light():
                 light = mcp.read_adc(2)
                 print "Light level is low\n"
                 GPIO.output(13, GPIO.HIGH)
-                time.sleep(0.5)
+                time.sleep(2)
                 GPIO.output(13, GPIO.LOW)
         #convert light to string for the sql query //i'm not sure if this is really needed
         slight = str(light)

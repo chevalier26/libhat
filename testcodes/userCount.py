fc1e577c002eb72fda5a327604781c54e127eee7
aldrinken <38404169+aldrinken@users.noreply.github.com>
2019-12-11 13:21:56 +0800

    Update userCount.py

--- testcodes/userCount.py
+++ testcodes/userCount.py
@@ -12,24 +12,49 @@ global delayChecker
 delayChecker = 0
 user_counter = 0
 
+	#connecting the database
+mydb = mysql.connector.connect(
+	 host="localhost",
+	 user="pi",
+	 passwd="raspberry",
+	 database="libhat"
+)
+
+print "Database Connected"
+mycursor = mydb.cursor()
+
+import datetime
+
 print "START SEQUENCE"
-time.sleep(5)
+time.sleep(3)
+
 
 while True:
 	i=GPIO.input(18)
 	if i==0:                 #When output from motion sensor is LOW
 		print "No intruders, (",i,")"
-		print user_counter
+		print "People: ", user_counter, "\n"
 		time.sleep(1)
 		delayChecker = 0
 
-	elif i==1:             #When output from motion sensor is HIGH
+	elif i==1:               #When output from motion sensor is HIGH
 		print "Intruder detected, (",i,")"
 		user_counter+=1
-		print "People: ", user_counter,# "\n"
+		print "People: ", user_counter, "\n"
 		time.sleep(2)
 		while i==1:
 			i=GPIO.input(18)
 			delayChecker+=1
-			print "[Delay:",delayChecker,"]", i, "\n"
-			time.sleep(1)	
+			print "[Delay:", delayChecker,"]", i,"\n"
+			time.sleep(1)
+
+	current_datetime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+	sql = "INSERT INTO user_count(datetime1, user_count, sensor_id) VALUES (%s,%s,%s)"
+	sensor_id=001
+	val = (current_datetime, user_counter, sensor_id)
+	mycursor.execute(sql, val)
+
+	mydb.commit()
+
+	print "Data transferred"
